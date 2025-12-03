efd833560e54d70404ba8b83d3f10a5ac939f53d
Julya Alves <130848710+JulyaAalves@users.noreply.github.com>
2025-12-03 17:35:08 -0300

    Add files via upload

---
+++ Atividade 2/receptor.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario F1</title>
+</head>
+<body>
+    <h1>Formulario F1</h1>
+    <a href="Atividade2.html">Home</a>
+    <pre>
+    <?php
+    print_r($_GET);
+    ?>
+    </pre>
+</body>
+</html>
